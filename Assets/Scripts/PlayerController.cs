d57ca074a33f926e29f1fe8ead9931dd48ea6e14
PoshSquash <57773399+PoshSquash@users.noreply.github.com>
2020-12-14 23:19:46 -0800

    Tutorial 4.3.5 complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     private GameObject focalPoint;
     public bool hasPowerup;
+    private float powerupStrength = 15.0f;
     
     // Start is called before the first frame update
     void Start()
@@ -36,7 +37,11 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
         {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
             Debug.Log("Player collided with " + collision.gameObject + " with powerup set to " + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
         }
 
 
